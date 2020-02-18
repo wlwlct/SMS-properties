937c7a5f444df5bb309608115dac512d2a1a328f
wlwlct <42161339+wlwlct@users.noreply.github.com>
2020-02-18 10:23:39 -0500

    Add comment

--- MoleculeProperities/MoleculePropertyDistribution.m
+++ MoleculeProperities/MoleculePropertyDistribution.m
@@ -1,17 +1,18 @@
+%Without threshold and normalization, histogram of max and average
+%wavelength of each moleucle (sum of molecule spectrum without normalization)
 %must not include data that not with same x value.
 clearvars
 solvent='F8T2400nmCH apd removed without consider marker';
- srdir=['/scratch/lwang74/PTU_spectrum_lifetime_bluehive/PTUdata/' solvent];
+srdir=['/scratch/lwang74/PTU_spectrum_lifetime_bluehive/PTUdata/' solvent];
 %srdir=['E:\02252019\dataset intermediates\0'];
 cd (srdir)
-
+wavelengthstart=1;
+edges=430:2:650;
 %Threshold_box=[0,1000;1001,2000;2001,3000;3001,4000;4001,5000;5001,6000;6001,7000;7001,250000];
 
 allnames=struct2cell(dir([ '*.mat']));
 [~,len]=size(allnames);
 Mspectrum=zeros(100,len+1);
-
-
 for len_i=1:1:len
     clear name
     name=char(allnames(1,len_i));
@@ -22,9 +23,10 @@ for len_i=1:1:len
     Mspectrum(:,1+len_i)=sum(datasetfile.dataset.ccdt(:,3:end),2);
     clearvars datasetfile
 end
-
-Mavewav= sum(Mspectrum(1:end,2:end).*Mspectrum(1:end,1),1)./sum(Mspectrum(1:end,2:end),1);
-[~,maxwavP]=max(Mspectrum(1:end,2:end),[],1);Mmaxwav=Mspectrum(maxwavP,1);
+%average wavelength for each molecule
+Mavewav= sum(Mspectrum(wavelengthstart:end,2:end).*Mspectrum(wavelengthstart:end,1),1)./sum(Mspectrum(wavelengthstart:end,2:end),1);
+%max wavelength for each molecule
+[~,maxwavP]=max(Mspectrum(wavelengthstart:end,2:end),[],1);Mmaxwav=Mspectrum(maxwavP+wavelengthstart-1,1);
 
 
 try
@@ -33,7 +35,7 @@ catch
     mkdir([srdir '/Molecule properties/']);
     cd([srdir '/Molecule properties/']);
 end
-edges=430:2:650;
+
 
 figure;
 histogram(Mavewav,edges);
