fbf634f9067f7be08725f4981176fb3f679deeac
wlwlct <42161339+wlwlct@users.noreply.github.com>
2020-02-20 12:42:42 -0500

    updata spectrum calculation

--- properties change with time/propertieschangewithtime.m
+++ properties change with time/propertieschangewithtime.m
@@ -155,7 +155,7 @@ subplot(1,2,1)
   surf(1:1:99,datasetfile.dataset.ccdt(:,1),timespectrum,'EdgeColor','none');colormap(jet);view([0 0 1]);
   title(['Spectrum (add up) change with time ' solvent])
 subplot(1,2,2)
-  surf(1:1:99,datasetfile.dataset.ccdt(:,1),normalize(timespectrum,1,'range'),'EdgeColor','none');colormap(jet);view([0 0 1]);
+  surf(1:1:99,datasetfile.dataset.ccdt(:,1),timespectrum./max(timespectrum(place:end,:),[],1),'EdgeColor','none');colormap(jet);view([0 0 1]);
   title(['Normalized Spectrum (add up) change with time ' solvent])
 saveas(gcf,[solvent ' Spectrum (add up) change with time.jpg']);
   saveas(gcf,[solvent ' Spectrum (add up) change with time.fig']);
@@ -167,7 +167,7 @@ subplot(1,2,1)
   surf(1:1:99,datasetfile.dataset.ccdt(:,1),timespectrum_normalized,'EdgeColor','none');colormap(jet);view([0 0 1]);
   title(['Spectrum (normalize then add up) change with time ' solvent])
 subplot(1,2,2)
-  surf(1:1:99,datasetfile.dataset.ccdt(:,1),normalize(timespectrum_normalized,1,'range'),'EdgeColor','none');colormap(jet);view([0 0 1]);
+  surf(1:1:99,datasetfile.dataset.ccdt(:,1),timespectrum_normalized./max(timespectrum_normalized(place:end,:),[],1),'EdgeColor','none');colormap(jet);view([0 0 1]);
   title(['Normalized Spectrum (normalize then add up) change with time ' solvent])
 saveas(gcf,[solvent ' Spectrum (normalize then add up) change with time.jpg']);
   saveas(gcf,[solvent ' Spectrum (normalize then add up) change with time.fig']);
